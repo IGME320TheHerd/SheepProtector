8a0e1da3cfb7b9d3b724d96fc31e4481ca28f0df
sacanthias <omibel7@gmail.com>
2026-02-18 22:25:01 -0500

    Completed basic UI framework
    NOTES:
    - current menu design is a WIP, not representative of the final product
    - quit button works within build context, not development environment

---
+++ SheepProtector/Assets/Scripts/Enums/ScreenState.cs
@@ -0,0 +1,17 @@
+/// 
+/// SCREENSTATE - written by Nao (sacanthias)
+/// PURPOSE - public enumeration for tracking game state changes as outlined in our FSM diagram
+/// LAST UPDATED - 2/18/26
+/// ________________________________________
+
+/// <summary>
+/// Details the different game states used in this program.
+/// </summary>
+public enum ScreenState
+{
+    Start,
+    Main,
+    Playing,
+    Paused,
+    End
+}
--- SheepProtector/Assets/Scripts/UIScenes/SceneManager.cs
+++ SheepProtector/Assets/Scripts/UIScenes/SceneManager.cs
@@ -1,25 +1,35 @@
+/// 
+/// SCENEMANAGER - written by Nao (sacanthias)
+/// PURPOSE - organize game state switches based off our FSM diagram
+/// LAST UPDATED - 2/18/26
+/// ________________________________________
+
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
-//public enum State
-//{
-//    Playing,
-//    Paused,
-//    Start,
-//    End
-//}
+using static UnityEngine.Rendering.DebugUI;
 
 public class SceneManager : MonoBehaviour
 {
+    // list of screens to reference later
+    [SerializeField] private List<GameObject> screens;
 
-    [SerializeField] GameObject pausePanel;
-    [SerializeField] GameObject settingsPanel;
+    // ScreenState enum checker +
+    // Property for the current screen state that may be referenced by other scripts
+    private ScreenState screenState;
 
+    public ScreenState ScreenState
+    {
+        get { return screenState; }
+    }
+    
     public static SceneManager Instance { get; private set; }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    // Singleton instance of SceneManager
+    private void Awake()
     {
         if (Instance == null)
         {
@@ -33,15 +43,102 @@ public class SceneManager : MonoBehaviour
         DontDestroyOnLoad(Instance);
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Start initializes the key fields that will be used for state switching later on
+    private void Start()
     {
-        
+        // screenState is set to start by default
+        screenState = ScreenState.Start;
+
+        // Fills the scenes list with each panel "screen"
+        // Each panel is the direct child of the Canvas element
+        screens = new List<GameObject>();
+
+        // Resets every panel in the scenes list to "hidden".
+        // This is the default state for panels.
+        foreach(GameObject panel in GameObject.FindGameObjectsWithTag("Screen"))
+        {
+            screens.Add(panel);
+            panel.SetActive(false);
+            Debug.Log("Added " + panel.name);
+        }
+
+        SwitchScreen("StartScreen");
+        Debug.Log("Switched to start screen");
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Quits the game application and closes the window.
+    /// </summary>
+    public void QuitGame()
     {
-        
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Toggles the Pause Screen on and off based off a button input.
+    /// </summary>
+    public void PauseToggle()
+    {
+        // checks the current screen state
+        if(screenState != ScreenState.Paused)
+        {
+            // timeScale = 0 essentially pauses in-game time and prevents the player from moving
+            Time.timeScale = 0;
+            SwitchScreen("PauseMenu");
+        }
+        else
+        {
+            Time.timeScale = 1;
+            SwitchScreen("Playing");
+            screenState = ScreenState.Playing;
+        }
+    }
+
+    /// <summary>
+    /// Switches to a specified screen based on the string passed.
+    /// </summary>
+    /// <param name="sceneName">The name of the screen to switch to (case/spelling sensitive)</param>
+    public void SwitchScreen(string screenName)
+    {
+        // nullable GameObject to avoid throwing a NullReferenceException error
+        GameObject? toChange = null;
+
+        // foreach loop goes through each GameObject in screens list and checks if they have the same name;
+        // hence why the 
+        // i'm aware this is kinda computationally expensive, but since it's called so sparingly, i did it anyway lol
+        foreach (GameObject panel in screens)
+        {
+            if (panel.name == screenName)
+            {
+                toChange = panel;
+            }
+
+            panel.SetActive(false);
+        }
+
+        // if statement checks if toChange is null to avoid throwing a NullReferenceException error
+        // setting timeScale to 0 essentially pauses in-game time and prevents the player from moving;
+        // here i use it as a failsafe of sorts
+        if (toChange != null)
+        {
+            toChange.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+
+        // this is really messy, but it basically checks the string passed through and changes the screen state accordingly
+        screenState = screenName switch
+        {
+            "StartScreen" => ScreenState.Playing,
+            "MainMenu" => ScreenState.Main,
+            "PauseMenu" => ScreenState.Paused,
+            "SettingsMenu" => ScreenState.Paused,
+            "EndScreen" => ScreenState.End,
+            // default case is used to set everything back to the playing state
+            _ => ScreenState.Playing,
+        };
     }
 }
