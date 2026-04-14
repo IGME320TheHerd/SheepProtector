ba0ac608e51151074908eee305be4cf3041a4eb0
Cameron Wilson <cameronwil.be@gmail.com>
2026-04-14 11:44:35 -0400

    Revert "Merge pull request #105 from IGME320TheHerd/revert-99-build-symposium-2"
    This reverts commit 586fd9eea124eb2ad54b286e8b76765409c42df2, reversing
    changes made to be486b7dd91c5770948f60732def824e6925aadb.

--- SheepProtector/Assets/Scripts/Animal/Sheepdog/Sheepdog.cs
+++ SheepProtector/Assets/Scripts/Animal/Sheepdog/Sheepdog.cs
@@ -61,15 +61,15 @@ public class Sheepdog : Animal
         //    barkHeldDown = false;
         //}
 
-        //if (barkVisTimer > 0.0f)
-        //{
-        //    barkVisual.enabled = true;
-        //    barkVisTimer -= Time.deltaTime;
-        //}
-        //else
-        //{
-        //    barkVisual.enabled = false;
-        //}
+        if (barkVisTimer > 0.0f)
+        {
+            barkVisual.enabled = true;
+            barkVisTimer -= Time.deltaTime;
+        }
+        else
+        {
+            barkVisual.enabled = false;
+        }
     }
 
     /// <summary>
--- SheepProtector/Assets/Scripts/Movement/PlayerController.cs
+++ SheepProtector/Assets/Scripts/Movement/PlayerController.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
@@ -13,20 +14,40 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float maxStamina = 100f;
     [SerializeField] private float usageRate = 15f;
     [SerializeField] private float regenRate = 10f;
+    [SerializeField] private GameObject barkSprite;
 
     Rigidbody rb;
     Vector2 moveInput = Vector2.zero;
     float currentSpeed;
     float currentStamina; //gets current stamina
+    Vector3 currentDir = Vector3.zero;
+    SpriteRenderer playerSprite;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerSprite = GetComponentInChildren<SpriteRenderer>(); 
     }
 
     private void FixedUpdate()
     {
+        Vector3 camForward = new Vector3(Camera.main.transform.forward.x, 0.0f, Camera.main.transform.forward.z);
+        float angle = Vector3.SignedAngle(currentDir, camForward, Vector3.up);
+
+        if (angle > 5 && angle < 175)
+        {
+            playerSprite.flipX = true;
+            barkSprite.transform.localPosition = new Vector3(-5.5f, 1.83f, 0.0f);
+            barkSprite.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (angle < -5 && angle > -175)
+        {
+            playerSprite.flipX = false;
+            barkSprite.transform.localPosition = new Vector3(5.5f, 1.83f, 0.0f);
+            barkSprite.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
         rb.angularVelocity = Vector3.zero;
         rb.linearVelocity = new Vector3(0, rb.linearVelocity.y, 0);
 
@@ -35,6 +56,7 @@ public class PlayerController : MonoBehaviour
         pos += moveInput.y * currentSpeed * Time.deltaTime * cam.forward;
         pos += moveInput.x * currentSpeed * Time.deltaTime * cam.right;
 
+        currentDir = pos;
         rb.MovePosition(pos);
     }
 
