a14859b79209b33c89f8d069d52320e4b21a8bd5
sacanthias <omibel7@gmail.com>
2026-04-30 15:20:19 -0400

    camera rotation brute force
    brute forced with legacy input manager. left shoulder (controller), left arrow key , and right arrow key work, right shoulder (controller) does not.

--- SheepProtector/Assets/Scripts/Camera/Camera.cs
+++ SheepProtector/Assets/Scripts/Camera/Camera.cs
@@ -1,10 +1,12 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class IsoCameraController : MonoBehaviour
 {
-    [SerializeField] private Transform target; // refernce for player        
+    public Transform target; // refernce for player        
     public float rotationSpeed = 5f; // how fast the camera rotates
+    //public InputActionMap cameraMap;
 
     private Camera cam; // refernce to camera
     private float targetYRotation; // refernce for roation angle
@@ -19,41 +21,11 @@ public class IsoCameraController : MonoBehaviour
     {
         transform.position = target.position; // snaps pivot to player position
 
-        //if (Input.GetKeyDown(KeyCode.LeftArrow)) targetYRotation -= 45f; // rotates left 45 degrees on q press
-        //if (Input.GetKeyDown(KeyCode.RightArrow)) targetYRotation += 45f; // rotates 45 degrees right on e press
-
-        //Debug.Log(targetYRotation);
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetButtonDown("CameraTurnLeft")) targetYRotation -= 45f; // rotates left 45 degrees on left arrow/shoulder press
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetButtonDown("CameraTurnRight")) targetYRotation += 45f; // rotates 45 degrees right on right arrow/shoulder press
 
         Quaternion targetRotation = Quaternion.Euler(0, targetYRotation, 0); // creates quaternion value using y axis, left or right
+
         transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed); // rotates from current rotation to target rotation 
-    }
-
-    void CameraUpdate(float y)
-    {
-        targetYRotation += y;
-        Quaternion targetRotation = Quaternion.Euler(0, targetYRotation, 0); // creates quaternion value using y axis, left or right
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed); // rotates from current rotation to target rotation 
-    }
-
-    public void OnCameraMoveLeft(InputAction.CallbackContext ctx)
-    {
-        if (ctx.started && !ctx.performed)
-        {
-            targetYRotation -= 45f;
-            Debug.Log(targetYRotation);
-            //CameraUpdate(-45f);
-           // Debug.Log("final movement: " + transform.rotation.ToString());
-        }
-    }
-
-    public void OnCameraMoveRight(InputAction.CallbackContext ctx)
-    {
-        if (ctx.started && !ctx.performed)
-        {
-            targetYRotation += 45f;
-            Debug.Log(targetYRotation);
-            //CameraUpdate(45f);
-          //  Debug.Log("final movement: " + transform.rotation.ToString());
-        }
     }
 }
